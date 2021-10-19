c80bc064aef2cfaad0c8aeda1b651c0ea5399507
jmapps <jmappsmuslim@gmail.com>
2021-10-19 15:38:38 +0300

    Added copy button state if text is empty

--- lib/widgets/copy_text_button.dart
+++ lib/widgets/copy_text_button.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:replacer/provider/main_content_text_field_state.dart';
 
 class CopyTextButton extends StatelessWidget {
   const CopyTextButton({Key? key}) : super(key: key);
@@ -8,7 +10,8 @@ class CopyTextButton extends StatelessWidget {
     return TextButton.icon(
       label: const Text('Копировать'),
       icon: const Icon(Icons.copy),
-      onPressed: () {},
+        onPressed: context.watch<MainContentTextFieldState>().getMainContentInputText.isNotEmpty ? () {
+        } : null
     );
   }
 }
